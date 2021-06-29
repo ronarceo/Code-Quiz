a3cad90d330acab563d52334aa71c19f0d1fd63b
ronarceo <ronaldmarceo@gmail.com>
2021-06-29 14:09:10 -0500

    modified script.js
    added questions and function to stop time once quiz is completed

--- assets/script.js
+++ assets/script.js
@@ -16,7 +16,70 @@ let timeLeft = document.querySelector("#timeLeft");
 let secondsLeft = 100;
 
 //variable defined to be used for question number
-let qNumber = 0
+let qNumber = 0;
+
+// if starBtn button clicked then quiz starts
+start.addEventListener('click', startQuiz)
+
+//function to start timer
+let timerInterval;
+function startTime() {
+  timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeLeft.textContent = secondsLeft;
+
+    if(secondsLeft === 0) {
+      alert('Game Over, you ran out of time.')
+      location.reload();
+    }
+  }, 1000);
+}
+
+//function to stop timer once all questions answered
+function stopTime() {
+  clearInterval(timerInterval);
+}
+
+//function to start quiz
+function startQuiz() {
+    rules.style.display = "none";
+    startMenu.style.display = "none";
+    quiz.style.display = "block";
+    startTime();
+    generateQuestion();
+}
+
+//function to generate question
+function generateQuestion() {
+  if (qNumber < qList.length) {
+    let q = qList[qNumber];
+    question.innerHTML = "<p>" + q.question + "</p>";
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+    choiceD.innerHTML = q.choiceD;
+  } else {
+    stopTime();
+  }
+}
+
+//function to select answer
+function choose(choice) {
+  let q = qList[qNumber];
+  if (choice == q.answer) {
+    qNumber++;
+    generateQuestion();
+  } else {
+    secondsLeft -= 5;
+  }
+}
+
+// if scoresBtn button clicked then shows lists of highscores
+scores.addEventListener('click', function(){
+    console.log('Highscores');
+    rules.style.display = "none";
+    startMenu.style.display = "none";
+})
 
 //array of questions
 var qList = [
@@ -37,66 +100,67 @@ var qList = [
     choiceD: "D) write.console()"
   },
   {
-    question: "",
-    answer: "",
-    choiceA: "A) ",
-    choiceB: "B) ",
-    choiceC: "C) ",
-    choiceD: "D) "
+    question: "In JavaScript, single line comments start with what symbols?",
+    answer: "A",
+    choiceA: "A) //",
+    choiceB: "B) /*",
+    choiceC: "C) */",
+    choiceD: "D) **"
   },
-];
-
-// if starBtn button clicked then quiz starts
-start.addEventListener('click', startQuiz)
-
-//function to start timer
-function startTime() {
-  var timerInterval = setInterval(function() {
-    timeLeft.textContent = secondsLeft;
-    secondsLeft--;
-
-    if(secondsLeft === 0) {
-      alert('Game Over, you ran out of time.')
-      location.reload();
-    }
-  }, 1000);
-}
-
-//function to start quiz
-function startQuiz() {
-    console.log('Quiz Started');
-    rules.style.display = "none";
-    startMenu.style.display = "none";
-    quiz.style.display = "block";
-    generateQuestion();
-    startTime();
-}
-
-//function to generate question
-function generateQuestion() {
-    console.log('Question Generated')
-    let q = qList[qNumber];
-    question.innerHTML = "<p>"+ q.question +"</p>";
-    choiceA.innerHTML = q.choiceA;
-    choiceB.innerHTML = q.choiceB;
-    choiceC.innerHTML = q.choiceC;
-    choiceD.innerHTML = q.choiceD;
-}
-
-//function to select answer
-function choose(choice) {
-  let q = qList[qNumber];
-  if (choice == q.answer) {
-    qNumber++;
-    generateQuestion();
-  } else {
-    secondsLeft = secondsLeft - 5;
+  {
+    question: "Which of the following is not a JavaScript data type?",
+    answer: "C",
+    choiceA: "A) string",
+    choiceB: "B) boolean",
+    choiceC: "C) subject",
+    choiceD: "D) object"
+  },
+  {
+    question: "What property or method returns the number of characters in a string?",
+    answer: "A",
+    choiceA: "A) length",
+    choiceB: "B) slice",
+    choiceC: "C) substr",
+    choiceD: "D) substring"
+  },
+  {
+    question: "Which method is used to add a value as the first item in an array without removing any of its current values?",
+    answer: "D",
+    choiceA: "A) .push()",
+    choiceB: "B) .pop()",
+    choiceC: "C) .shift()",
+    choiceD: "D) .unshift()"
+  },
+  {
+    question: "The method Math.random() returns a random number between what numbers?",
+    answer: "B",
+    choiceA: "A) -1 and 0",
+    choiceB: "B) 0 and 1",
+    choiceC: "C) 0 and 10",
+    choiceD: "D) 0 and 100"
+  },
+  {
+    question: "Which comparison returns a value of true?",
+    answer: "C",
+    choiceA: "A) 2 < 12",
+    choiceB: "B) '2' > '12'",
+    choiceC: "C) Both A and B",
+    choiceD: "D) Neither A and B"
+  },
+  {
+    question: "In an if statement the condition is enclosed between what?",
+    answer: "A",
+    choiceA: "A) ()",
+    choiceB: "B) {}",
+    choiceC: "C) []",
+    choiceD: "D) ''"
+  },
+  {
+    question: "Which of the following is a type of loop in JavaScript?",
+    answer: "D",
+    choiceA: "A) if",
+    choiceB: "B) for",
+    choiceC: "C) while",
+    choiceD: "D) Both B and C"
   }
-}
-
-// if scoresBtn button clicked then shows lists of highscores
-scores.addEventListener('click', function(){
-    console.log('Highscores');
-    rules.style.display = "none";
-    startMenu.style.display = "none";
-})
+];
